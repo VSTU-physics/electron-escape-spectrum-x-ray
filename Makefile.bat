9cee3bc60579ef10bf0bcaca081b8c515c158843
sputnikas <sputnik_as@mail.ru>
2014-11-18 22:46:53 +0400

    Изм2.

---
+++ Makefile.bat
@@ -0,0 +1,8 @@
+cd src\
+mingw32-make -fMakefile.make
+move *.exe ..\
+Rem mingw32-make -fMakefile.make clean
+cd ..\
+pause
+main.exe
+pause
---
+++ src/parse.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdio.h>
+#include "physics.h"
+
+void test_parse();
+void load(int Z, const char* ch, auger_t &aug);
