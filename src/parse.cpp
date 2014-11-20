178e4b40c0eff3dae08d57c4d584f4f43d47185e
sputnikas <sputnik_as@mail.ru>
2014-11-20 16:03:07 +0400

    Парсим

--- src/parse.cpp
+++ src/parse.cpp
@@ -7,14 +7,18 @@ void load_auger(int Z, const char* ch, auger_t &aug)
 	FILE *fd;
 	if ((fd = fopen(ch, "r")) == NULL)
 	{
-        printf("Не могу открыть файл %s. Проверьте, что файл существует\n", ch);
+        printf("Can't open file %s. Check that file exists\n", ch);
         return;
     };
 	fscanf(fd, "%s\n", chrm);
-    while (z!=Z)
+	aug.E = new double[1];
+	aug.P = new double[1];
+    do 
 	{
         fscanf(fd, "%d ", &z);
 		fscanf(fd, "%d", &aug.N);
+		delete aug.E;
+		delete aug.P;
 		aug.E = new double[aug.N];
 		aug.P = new double[aug.N];
 		fscanf(fd, " [");
@@ -32,10 +36,10 @@ void load_auger(int Z, const char* ch, auger_t &aug)
 		fscanf(fd, "%s%s\n", aug.atom, aug.shell);
 		if ((feof(fd)!=0)&&(z!=Z)) 
 		{
-			printf("Элемент отсутствует в списке\n", ch);
+			printf("Element is absent at list\n", ch);
 			return;
 		}
-	}
+	} while (z!=Z);
 	fclose(fd);
 }
 
@@ -44,30 +48,30 @@ void load_subst(int Z, const char* ch, subst_t &subs){
 	FILE *fd;
 	if ((fd = fopen(ch, "r")) == NULL)
 	{
-        printf("Не могу открыть файл %s. Проверьте, что файл существует\n", ch);
+        printf("Can't open file %s. Check that file exists\n", ch);
         return;
     };
 	fscanf(fd, "%s\n", chrm);
-    while (subs.Z!=Z)
+    do 
 	{
-        fscanf(fd, "%d %lf %lf %lf %s\n", &subs.Z, &subs.M, &subs.rho, &subs.U0);
+        fscanf(fd, "%d %lf %lf %lf %s\n", &subs.Z, &subs.M, &subs.rho, &subs.U0, chrm);
 		if ((feof(fd)!=0)&&(subs.Z!=Z)) 
 		{
-			printf("Элемент отсутствует в списке\n", ch);
+			printf("Element is absent at list\n", ch);
 			return;
 		}
-	}
+	} while (subs.Z!=Z);
 	fclose(fd);	
 };
 
-void load_ltr(double *ltr, double *E, int N, const char* ch, int Z)
+void load_ltr(double *ltr, double *E, int N, const char* ch, subst_t s)
 {
 	char chr, chrm[500], filename[50];
-	sprintf(filename, "%s%d_el.pl", ch, Z);
+	sprintf(filename, "%s%d_el.pl", ch, s.Z);
 	FILE *fd;
 	if ((fd = fopen(filename, "r")) == NULL)
 	{
-        printf("Не могу открыть файл %s. Проверьте, что файл существует\n", filename);
+        printf("Can't open file %s. Check that file exists\n", filename);
         return;
     };
 	int theta_l, e_l;
@@ -86,29 +90,68 @@ void load_ltr(double *ltr, double *E, int N, const char* ch, int Z)
 		for (int j = 0; j<theta_l; j++)
 		{
 			fscanf(fd, "%lE", &dltr[j]);
-			dltr[j]*=(1 - cos(theta[j]))*sin(theta[j]);
+			dltr[j]*=s.rho*Na/s.M*(1 - cos(theta[j]))*sin(theta[j]);
 		}
 		fscanf(fd, "%s\n", chrm);
 		ltr_points[e_l - 1 - i] = 2*M_PI*int_cubic_spline(theta[0], theta[theta_l-1], theta, dltr, theta_l);
 	}
-	FILE *ftest;
-	ftest = fopen("test.gp", "w");
-	fprintf(ftest, "plot '-' with lines\n");
-	for (int i = 0; i<e_l; i++)
-	{
-		fprintf(ftest, "%e %e\n", E_points[i], ltr_points[i]);
-	}
-	fclose(ftest);
-	ftest = popen("test.gp", "w");
-	pclose(ftest);
 	for (int i = 0; i<N; i++)
 	{
 			E[i] = E_points[0] + (E_points[e_l - 1] - E_points[0])/(N-1)*i;
 	}
 	eval_cubic_spline(E, ltr, N, E_points, ltr_points, e_l);
-}
+	fclose(fd);
+};
 
-void test_parse(){
+void load_esharp(double *esharp, double *E, int N, const char* ch, subst_t s)
+{
+	char chr, chrm[500], filename[50];
+	sprintf(filename, "%s%d_in.pl", ch, s.Z);
+	FILE *fd;
+	if ((fd = fopen(filename, "r")) == NULL)
+	{
+        printf("Can't open file %s. Check that file exists\n", filename);
+        return;
+    };
+	int e_l;
+	fscanf(fd, "%s\n", chrm);
+	fscanf(fd, "%s\n", chrm);
+	fscanf(fd, "%s\n", chrm);
+	double Q_points[500], dW_points[500], E_points[100], e_sharp[100];
+	int i = 0, j, jmax, imax; 
+	do 
+	{	
+		fscanf(fd, "%lE", &E_points[i]);
+		j = 0;
+		do
+		{
+			fscanf(fd, "%lE", &Q_points[j]);
+			fscanf(fd, "%c", &chr);
+			j++;
+		} while (chr!='\n');
+		j = 0;
+		do
+		{
+			fscanf(fd, "%lE", &dW_points[j]);
+			fscanf(fd, "%c", &chr);
+			j++;
+			if (chr!='\n') printf("%e\n", dW_points[j]);
+		} while (chr!='\n');
+		jmax = j;
+		e_sharp[i] = 0;
+		for (int l = 1; l<jmax; l++)
+		{
+			e_sharp[i] += 0.5*(Q_points[l] - Q_points[l-1])*(Q_points[l]*dW_points[l] + Q_points[l-1]*dW_points[l-1]);
+		}
+		i++;
+	} while ((feof(fd)==0)||(chr=='#'));	
+	fclose(fd);
+	imax = i;
+	eval_cubic_spline(E, esharp, N, E_points, e_sharp, imax);
+};
+
+void test_parse()
+{
 	auger_t aug;
 	int Z = 14;
 	load_auger(Z, "data/aug.pl", aug);
@@ -126,11 +169,14 @@ void test_parse(){
 	printf("%s \n", aug.atom);
 	printf("%s \n", aug.shell);
 	int N = 10;
-	double ltr[N], E[N];
-	load_ltr(ltr, E, N, "data/", 32);
+	double ltr[N], E[N], esharp[N];
+	subst_t s;
+	load_subst(32, "data/subst.pl", s);
+	load_ltr(ltr, E, N, "data/", s);
+	load_esharp(esharp, E, N, "data/", s);
 	for (int i = 0; i<N; i++)
 	{
-		printf("%e %e\n", E[i], ltr[i]);
+		printf("%e %e\n", E[i], ltr[i], s);
 	}
 	
-}
+};
--- src/parse.h
+++ src/parse.h
@@ -8,3 +8,4 @@
 void test_parse();
 void load_auger(int Z, const char* ch, auger_t &aug);
 void load_subst(int Z, const char* ch, subst_t &subs);
+void load_esharp(double *esharp, double *E, int N, const char* ch, subst_t s);
