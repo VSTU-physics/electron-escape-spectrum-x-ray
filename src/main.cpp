c7f410550bddc3656c6cbc60994502081fbdaf73
Vova Abdrakhmanov <369565@gmail.com>
2014-11-26 20:22:16 +0300

    Учёл то, что внутри материала кинетическая энергия K = E + U0

--- src/main.cpp
+++ src/main.cpp
@@ -34,52 +34,70 @@ int main()
 
 	load_subst(Z, "data/subst.pl", s);
 	load_auger(Z, "data/aug.pl", a);
+    printf("Загружены данные для Z = %d:\n", Z);
+    printf("Свойства материала:\n");
+    printf("Зарядовое число: %d\n", s.Z);
+    printf("Плотность: %f\n", s.rho);
+    printf("Молярная масса: %f\n", s.M);
+    printf("Макс. энергия Оже-электронов: %f\n", s.Emax);
+    printf("Работа выхода: %f\n", s.U0);
+    printf("\n");
+    printf("Характеристики электронов:\n");
+    printf("Кол-во типов: %d\n", a.N);
+    printf("Энергии: [");
+    for (int i = 0; i < a.N; i++) {
+        printf(" %f", a.E[i]);
+    }
+    printf(" ]\n");
+    printf("Вероятности: [");
+    for (int i = 0; i < a.N; i++) {
+        printf(" %f", a.P[i]);
+    }
+    printf(" ]\n");
 	//load_ltr(ltr, E, M, "data/", s);
 	//load_esharp(eps, E, M, "data/", s);
 
 	double dE = a.E[0] / (M - 1);
-	for (int i = 0; i<M; i++)
-        E[i] = i * dE;
-
 	FILE *fd;
     fd = fopen("trash.out", "w");
-	for (int i = M - 1; i>=0; i--)
-	{
-		tmp = u;
-		u = up;
-		up = tmp;
+	for (int i = 0; i<M; i++)
+    {
+        E[i] = i * dE;
+        u[i] = a.P[0];
+    }
 
-        for (int j = 0; j < N; j++) {
-            fprintf(fd, "%f ", u[i]);
+    for (int i = M - 1; i>=0; i--)
+    {
+        tmp = u;
+        u = up;
+        up = tmp;
+
+        source = 0;
+        for (int k = 1; k<a.N; k++)
+        {
+            source += a.P[k]*delta(E[i] - a.E[k], dE);
         }
-        fprintf(fd, "\n");
-
-		source = 0;
-		for (int k = 0; k<a.N; k++)
-		{
-			source += a.P[k]*delta(E[i] - a.E[k], dE);
-		}
-		spe(u, up, z, N, dE,
-			- 1./(3*l_tr(s, E[i]) * eps(s, E[i])),
-			source,
-			I1(s, E[i])/(2 - 3 * I2(s, E[i])),
-			- 1./l_tr(s, E[i])/3,
-			0,
-			1.,
-			0.,
-			0.
-			);
-		f[i] = l_tr(s, E[i])*3*I1(s, E[i])/(2 - 3 * I2(s, E[i]))*u[0];
-	}
+        spe(u, up, z, N, dE,
+            - 1./(3*l_tr(s, E[i]) * eps(s, E[i])),
+            source,
+            I1(s, E[i])/(2 - 3 * I2(s, E[i])),
+            - 1./l_tr(s, E[i])/3,
+            0,
+            1.,
+            0.,
+            0.
+            );
+        f[i] = l_tr(s, E[i])*3*I1(s, E[i])/(2 - 3 * I2(s, E[i]))*u[0];
+    }
     fclose(fd);
-	fd = fopen("data.gp", "w");
-	fprintf(fd, "plot '-' with lines\n");
-	for (int i = M - 1; i>=0; i--)
-	{
-		fprintf(fd, "%e %e\n", E[i], f[i]);
-	}
+    fd = fopen("data.gp", "w");
+    fprintf(fd, "plot '-' with lines\n");
+    for (int i = M - 1; i>=0; i--)
+    {
+        fprintf(fd, "%e %e\n", E[i], f[i]);
+    }
 	fclose(fd);
-	fd = popen("gnuplot -p data.gp", "w");
-	pclose(fd);
+	//fd = popen("gnuplot -p data.gp", "w");
+	//pclose(fd);
 	return 0;
 }
--- src/physics.cpp
+++ src/physics.cpp
@@ -11,21 +11,21 @@ double J(int Z)
 
 double eps(subst_t s, double E)
 {
-    double T = E / Ee;
+    double T = (s.U0 + E) / Ee;
     return 4 * M_PI * s.rho / s.M * Na * s.Z * pow((T+1)*re, 2) /
         T / (T + 2) * log(1.166 * E / J(s.Z));
 }
 
 double eta(subst_t s, double E)
 {
-    double T = E / Ee;
+    double T = (s.U0 + E) / Ee;
     return .25 * pow(alpha * pow(s.Z, 1./3) / 0.885, 2) / T / (T + 2) *
         (1.13 + 3.76 * pow(s.Z * alpha * (T + 1), 2) / T / (T + 2));
 }
 
 double l_tr(subst_t s, double E)
 {
-    double T = E / Ee;
+    double T = (s.U0 + E) / Ee;
     return 2 * M_PI * s.rho / s.M * Na * s.Z * (s.Z + 1) *
         pow((T+1)*re/T/(T+2), 2) *
         (log(1 + 1 / eta(s, E)) - 1 / (eta(s, E) + 1));
@@ -33,14 +33,14 @@ double l_tr(subst_t s, double E)
 
 double I1(subst_t s, double E)
 {
-    double eps = sqrt(s.U0 / E);
+    double eps = sqrt(s.U0 / (s.U0 + E));
     double e = eps * eps;
     return -4. / 3 / e / e * (1 - pow(1 - e, 1.5)) + 2 / e - 2 * e / 3;
 }
 
 double I2(subst_t s, double E)
 {
-    double eps = sqrt(s.U0 / E);
+    double eps = sqrt(s.U0 / (s.U0 + E));
     double e = eps * eps;
     double i = 1 - e;
     return 4. / e / e * pow(i, 1.5) * (1. / 3 - i / 15 - i * i / 21) -
