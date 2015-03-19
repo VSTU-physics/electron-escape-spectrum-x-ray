03261b5610e9e1113222f804ca913c3fd0dce6eb
sputnikas <sputnik_as@mail.ru>
2015-03-19 11:01:26 +0300

    [doc] Первый тест doxygen
    - Doxyfile изменён: теперь видны функции и их описание.
    - Тестовая документация для calculations.cpp

--- src/calculations.cpp
+++ src/calculations.cpp
@@ -1,3 +1,8 @@
+/*!
+    \file
+    Файл содержит вспомогательные математические функции.
+*/
+
 #include <cstdio>
 #include <cmath>
 #include <cstdlib>
@@ -5,20 +10,75 @@
 #include "calculations.h"
 
 /*!
+    \brief
 	spe решает уравнение:
 	\f[
 		\frac{\partial f}{\partial t} =
-		p(z, t) \frac{\partial^2 f}{\partial z^2} + g(z, t),
+		p(t) \frac{\partial^2 f}{\partial z^2} + g(t),
 	\f]
 	при граничных условиях:
 	\f[
-		\a_1(t) f + \b_1(t) \frac{\partial f}{\partial z} \Bigg|_{z = z_1} = \c_1(t),
+		a_1(t) f + b_1(t) \frac{\partial f}{\partial z} \Bigg|_{z = z_1} = c_1(t),
 	\f]
 	\f[
-		\a_2(t) f + \b_2(t) \frac{\partial f}{\partial z} \Bigg|_{z = z_2} = \c_2(t),
+		a_2(t) f + b_2(t) \frac{\partial f}{\partial z} \Bigg|_{z = z_2} = c_2(t),
 	\f]
-	и произвольных начальных условиях неявным методом на одном шаге. Ошибка $O(h)$. A, B, C -- диагонали
+	и произвольных начальных условиях неявным методом на одном шаге \f$[t, t+dt]\f$.
+
+	\details
+	\param[out] f Содержит новый результат решения уравнения на одном шаге
+	\param[in] f_prev Содержит предыдущий результат решения уравнения на одном шаге
+	\param[in]  z Массив узлов, над которыми решается уравнение
+	\param[in]  N Число узлов
+	\param[in] dt Шаг по времени (энергии)
+    \param[in] pt Параметр \f$p(t)\f$
+    \param[in] gt Параметр \f$g(t)\f$
+    \param[in] a_1t Параметр \f$a_1(t)\f$
+    \param[in] b_1t Параметр \f$b_1(t)\f$
+    \param[in] c_1t Параметр \f$c_1(t)\f$
+    \param[in] a_2t Параметр \f$a_2(t)\f$
+    \param[in] b_2t Параметр \f$b_2(t)\f$
+    \param[in] c_2t Параметр \f$c_2(t)\f$
+
+	Код:
+	\code
+        A[0] = 0;
+        B[0] = a_1t - b_1t/(z[1] - z[0]);
+        C[0] = b_1t/(z[1] - z[0]);
+        D[0] = c_1t;
+        for (int i = 1; i < N - 1; i++)
+        {
+            double dz1, dz2, dza;
+            dz1 = z[i] - z[i-1];
+            dz2 = z[i+1] - z[i];
+            dza = (dz1 + dz2)*0.5;
+            A[i] = dt*pt/dz1/dza;
+            B[i] = - 1 - 2*pt/dz1/dz2*dt;
+            C[i] = dt*pt/dz2/dza;
+            D[i] = - gt*dt - f_prev[i];
+        }
+        A[N-1] = - b_2t/(z[N-1] - z[N-2]);
+        B[N-1] = a_2t + b_2t/(z[N-1] - z[N-2]);
+        C[N-1] = 0;
+        D[N-1] = c_2t;
+	\endcode
+	задаёт трёхдиагональную матрицу и правую часть уравнения. A, B, C -- диагонали
 	матрицы нижняя, собственно диагональ, верхняя. D -- правая часть уравнения.
+	Код:
+	\code
+        for (int i = 1; i<N; i++)
+        {
+            B[i] -= A[i]/B[i-1]*C[i-1];
+            D[i] -= A[i]/B[i-1]*D[i-1];
+        }
+        f[N-1] = D[N-1]/B[N-1];
+        for (int i = N-2; i>=0; i--)
+        {
+            //if (fabs(f_prev[i]) > 0) printf("%e %e\n", f[i], f_prev[i]);
+            f[i] = (D[i] - C[i] * f[i+1])/B[i];
+        }
+	\endcode
+	Реализует метод прогонки решения систем линейных уравнений с трёхдиагональными матрицами.
 */
 
 void spe(double *f,
@@ -69,14 +129,66 @@ void spe(double *f,
     //if (fabs(f_prev[1]) > 0) getchar();
 }
 
-/*
-	eval_cubic_spline заносит значения в точках xs[j] в массив ys[j], j = 0, 1, 2, ..., M-1 функции заданной кубиче-
-	скими сплайнами для таблицы (x[i], y[i]), i = 0, 1, 2, ... , N-1. int_cubic_spline ищет интегралы, cubic_spline
-	ищет коэффициенты сплайна:
-	\[
+/*!
+    \brief
+	cubic_spline ищет коэффициенты сплайна:
+	\f[
 		S_i(x) = a_i + b_i (x - x_i) + \frac{1}{2} c_i (x - x_i)^2 + \frac{1}{6} d_i (x - x_i)^3
 		\quad x\in[x_i, x_{i+1}].
-	\]
+	\f]
+
+	\details
+	\param[out] x Точки \f$ x_i \f$, в которых нам известны значения функции \f$ y_i \f$
+	\param[out] y Массив \f$ y_i \f$
+	\param[out] a Массив коэффициентов \f$ a_i \f$
+	\param[out] b Массив коэффициентов \f$ b_i \f$
+	\param[out] c Массив коэффициентов \f$ c_i \f$
+	\param[out] d Массив коэффициентов \f$ d_i \f$
+
+	Решение задачи сводится к решению системы линейных уравнений с трёхдиагональной матрицей относительно вектора \f$ c_i \f$. Код:
+	\code
+        double A[N-2], B[N-2], C[N-2], D[N-2];
+        double dx[N-1], dydx[N-1];
+        c[0] = 0;
+        c[N-1] = 0;
+        dx[0] = x[1] - x[0];
+        dydx[0] = (y[1] - y[0])/dx[0];
+        for (int i = 0; i<(N-2); i++)
+        {
+            dx[i+1] = x[i+2] - x[i+1];
+            dydx[i+1] = (y[i+2] - y[i+1])/dx[i+1];
+            A[i+1] = dx[i];
+            B[i] = 2*(dx[i] + dx[i+1]);
+            C[i] = dx[i];
+            D[i] = 6*(dydx[i+1] - dydx[i]);
+        }
+        for (int i = 1; i<N-2; i++)
+        {
+            B[i] -= A[i]/B[i-1]*C[i];
+            D[i] -= A[i]/B[i-1]*D[i-1];
+        }
+        c[N-2] = D[N-3]/B[N-3];
+        for (int i = N-4; i>=0; i--)
+        {
+            c[i+1] = (D[i] - C[i] * c[i+2])/B[i];
+        }
+	\endcode
+	Остальные значения коэффициентов могут быть найдены как линейные комбинации \f$ c_i \f$, что реализует следующий код:
+	\code
+        for (int i = 0; i<N-1; i++)
+        {
+            a[i] = y[i];
+            b[i] = dydx[i] - c[i]*dx[i]/3 - c[i+1]*dx[i]/6;
+            d[i] = (c[i+1] - c[i])/dx[i];
+        }
+	\endcode
+	Обратите внимание 3 коэффициента остаются произвольными и их выбор связан с тем, как мы задаём приближение на концах.
+	В данном случае они выбраны следующими:
+	\code
+        d[N-1] = 0;
+        b[N-1] = 0;
+        a[N-1] = y[N-1];
+	\endcode
 */
 
 void cubic_spline(double *x, double *y, int N, double *a, double *b, double *c, double *d)
